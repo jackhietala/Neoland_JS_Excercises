42b47754023272591ed231bde14e3efd0435b73d
jackhietala <jacobolinares10@gmail.com>
2023-01-10 23:15:20 +0100

    Iteration#1

--- JS_Basic_Algorithms.js
+++ JS_Basic_Algorithms.js
@@ -1,1 +1,13 @@
 // Iteración #1: Variables
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+
+// Iteración #2: Variables avanzadas
+
+const character = {name: 'Jack Sparrow', age: 10};
+age: 25;
+console.log(age)
